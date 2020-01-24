ede0bb47a5cb435509cb8f8eff8b369f2fab4292
LAPTOP-0I0SFG2M\kimhanui <cocacola2017@naver.com>
2020-01-24 11:25:43 +0900

    [EHN] work-3
    - 주석 제거

--- working/src/main/java/ProjectMain.java
+++ working/src/main/java/ProjectMain.java
@@ -5,9 +5,3 @@ public class ProjectMain {
         Function func = new Function();
     }
 }
-/**
- * 1. CalculateDto 만들어서 사용
- * 2. getter/setter 이용
- * 3. main - 계산기 객체 생성, 생성자를 통해 동작 수행
- * 4. 캡슐화
- */
