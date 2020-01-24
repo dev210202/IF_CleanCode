6fc619b9684251da676968bd92f6b549b00af895
LAPTOP-0I0SFG2M\kimhanui <cocacola2017@naver.com>
2020-01-24 11:04:27 +0900

    [EHN] work-3
    - 클래스 이름 카멜식+첫글자는 대문자로 변경
    
    - float-> double 사용

--- working/src/main/java/ProjectMain.java
+++ working/src/main/java/ProjectMain.java
@@ -2,8 +2,12 @@ public class ProjectMain {
 
     public static void main(String[] args) {
 
-        function func = new function();
-
-        func.calculateAndPrint();
+        Function func = new Function();
     }
 }
+/**
+ * 1. CalculateDto 만들어서 사용
+ * 2. getter/setter 이용
+ * 3. main - 계산기 객체 생성, 생성자를 통해 동작 수행
+ * 4. 캡슐화
+ */
